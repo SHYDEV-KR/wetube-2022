2888cff3e9da3f0efe89111b2eb31e3f53f7120d
paulyoon28 <ekdmbzzang@naver.com>
2022-06-24 21:12:22 +0900

    #4.2 Cleaner Code : Routers

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 4000;
 
@@ -7,23 +10,6 @@ const app = express();
 const logger = morgan("dev");
 
 app.use(logger);
-
-// 라우터 생성
-
-// Home
-const globalRouter = express.Router();
-const handleHome = (req, res) => res.send("Home");
-globalRouter.get("/", handleHome);
-
-// User
-const userRouter = express.Router();
-const handleEditUser = (req, res) => res.send("Edit User");
-userRouter.get("/edit", handleEditUser);
-
-// Video
-const videoRouter = express.Router(); // #2
-const handleWatchVideo = (req, res) => res.send("Watch Video"); // #4
-videoRouter.get("/watch", handleWatchVideo); // #3
 
 // Middlewares
 app.use("/", globalRouter);
