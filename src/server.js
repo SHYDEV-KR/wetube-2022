7ec5e600d03f17092d207229aec2194408bbcd73
paulyoon28 <ekdmbzzang@naver.com>
2022-06-24 16:29:06 +0900

    #3.5 basic middleware

--- src/server.js
+++ src/server.js
@@ -4,11 +4,16 @@ const PORT = 4000;
 
 const app = express();
 
+const gossipMiddleware = (req, res, next) => {
+    console.log(`Someone is going to: ${req.url}`);
+    next();
+}
+
 const handleHome = (req, res) => {
     return res.send("Welcome home!");
 };
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 
 const handleLogin = (req, res) => {
     return res.send("This is the Login Page!");
