ab6102af55349a5e2989c399e551ee5f9376b398
paulyoon28 <ekdmbzzang@naver.com>
2022-06-24 16:38:42 +0900

    #3.6 middleware : app.use

--- src/server.js
+++ src/server.js
@@ -4,16 +4,17 @@ const PORT = 4000;
 
 const app = express();
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`Someone is going to: ${req.url}`);
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
     next();
-}
+  };
 
 const handleHome = (req, res) => {
     return res.send("Welcome home!");
 };
 
-app.get("/", gossipMiddleware, handleHome);
+app.use(logger);
+app.get("/", handleHome);
 
 const handleLogin = (req, res) => {
     return res.send("This is the Login Page!");
