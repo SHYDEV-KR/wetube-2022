05eaad5668a8270b45108e1dd1251c0e5c9c327c
paulyoon28 <ekdmbzzang@naver.com>
2022-06-24 23:20:47 +0900

    #4.3 Exports, Separating Router - Controller

--- src/server.js
+++ src/server.js
@@ -11,9 +11,9 @@ const logger = morgan("dev");
 
 app.use(logger);
 
-// Middlewares
+
 app.use("/", globalRouter);
-app.use("/videos", videoRouter); // #1
+app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
 const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
