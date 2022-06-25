772faf117720734438ae0bb95ff4139fad0606ff
paulyoon28 <ekdmbzzang@naver.com>
2022-06-25 13:16:08 +0900

    [FIX]#5.0, #5.1 returning HTML : PUG & PUG settings

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,3 +1,6 @@
+import express from "express";
+import { see, edit, upload, deleteVideo } from "../controllers/videoController";
+
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", see);
