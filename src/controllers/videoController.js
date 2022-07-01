1e3d7afd154447ddc9b6002793dace167aa9f15b
SHY <ekdmbzzang@naver.com>
2022-07-01 21:41:45 +0900

    #5.7 PUG Conditionals

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,14 @@
-export const edit = (req, res) => {
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
-export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
+const fakeUser = {
+    username: "Nicolas",
+    loggedIn: false,
+};
+
+export const trending = (req, res) =>
+res.render("home", { pageTitle: "Home", fakeUser });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => {
-    return res.send("Delete Video");
+return res.send("Delete Video");
 };
