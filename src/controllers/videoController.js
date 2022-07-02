9aa9f00ba4e3c175eb99dac751dd50d82440f948
SHY <ekdmbzzang@naver.com>
2022-07-02 09:19:25 +0900

    #6.0, #6.1 Array DB

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,4 @@
-export const trending = (req, res) => {
-    const videos = [
+let videos = [
     {
         title: "First Video",
         rating: 5,
@@ -14,7 +13,7 @@ export const trending = (req, res) => {
         comments: 2,
         createdAt: "2 minutes ago",
         views: 59,
-        id: 1,
+        id: 2,
     },
     {
         title: "Third Video",
@@ -22,15 +21,19 @@ export const trending = (req, res) => {
         comments: 2,
         createdAt: "2 minutes ago",
         views: 59,
-        id: 1,
+        id: 3,
     },
-    ];
-return res.render("home", { pageTitle: "Home", videos });
+];
+
+export const trending = (req, res) => {
+    return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => res.render("watch");
+export const watch = (req, res) => {
+    const { id } = req.params;
+    const video = videos[id - 1];
+    res.render("watch", { pageTitle: `Watching ${video.title}`, video })
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => {
-return res.send("Delete Video");
-};
+export const deleteVideo = (req, res) => res.send("Delete Video");
