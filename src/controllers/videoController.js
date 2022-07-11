eb1967ed838f54cdb6c3cd012e518e4a61f76485
SHY <ekdmbzzang@naver.com>
2022-07-11 20:20:26 +0900

    #6.20 404 Video Not found Page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -8,6 +8,9 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", { pageTitle: "Video not Found.." });
+    }
     return res.render("watch", { pageTitle: video.title, video });
 };
 
