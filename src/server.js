a5dd5794945d1b6af448192452f934d76a8316ae
SHY <ekdmbzzang@naver.com>
2022-07-12 21:14:41 +0900

    #7.7, #7.8 Sessions and Cookies

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 
 import globalRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,6 +13,19 @@ app.set("view engine", "pug")
 app.set("views", process.cwd() + "/src/views")
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+}))
+
+app.use((req, res, next) => {
+    req.sessionStore.all((error, sessions) => {
+        console.log(sessions);
+        next();
+    })
+});
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
