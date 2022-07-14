75454d21ec3e0320251a8a528d01b0b9cb37f4f9
SHY <ekdmbzzang@naver.com>
2022-07-14 12:13:27 +0900

    [REFACT] Github Login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -39,7 +39,7 @@ export const getLogin = (req, res) => res.render("login", { pageTitle: "Login" }
 export const postLogin = async (req, res) => {
     const { username, password } = req.body;
     const pageTitle = "Login"
-    const user = await User.findOne({ username });
+    const user = await User.findOne({ username, socialOnly: false });
     if (!user) {
         return res.status(400).render("login", {
             pageTitle,
@@ -108,26 +108,20 @@ export const finishGithubLogin = async (req, res) => {
             return res.redirect("/login");    
         }
 
-        const existingUser = await User.findOne({ email: emailObj.email });
-        if (existingUser) {
-            req.session.loggedIn = true;
-            req.session.user = existingUser;
-            return res.redirect("/");
-        } else {
-            // create account
-            const user = await User.create({
+        let user = await User.findOne({ email: emailObj.email });
+        if (!user) {
+            user = await User.create({
                 name: userData.name,
                 username: userData.login,
                 email: emailObj.email,
                 password: "",
                 socialOnly: true,
                 location: userData.location,
-            })
-            req.session.loggedIn = true;
-            req.session.user = user;
-            return res.redirect("/");
+            });
         }
-
+        req.session.loggedIn = true;
+        req.session.user = user;
+        return res.redirect("/");
     } else {
         return res.redirect("/login");
     }
